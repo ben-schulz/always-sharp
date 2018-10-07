41026bc6feeb00fae80ed8bd8874f3eda79ce100
ben-schulz <benjamin.john.schulz@gmail.com>
2018-10-06 23:29:05 -0500

    doc(git): comment on 'git_undo'.
    So that there is no confusion
    regarding its limitations.

--- git_undo.sh
+++ git_undo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# undo all modifications to existing, tracked files in git.
+#
+# (will NOT undo add/rm/mv operations)
+#
+# useful as a quick rewind when doing small refactors.
+
 git checkout -- $(
     git status --short |
     sed --quiet --expression '/^ M /{s/ M //p}' |
